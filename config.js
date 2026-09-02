2316d95d03a650db9d6445a61f5c6cea4fd7d4ae
ชมรมนักสังคมสงเคราะห์และนักจิตวิทยา ป.วิ.อาญา <cpc.psw.thailand@gmail.com>
2026-09-02 12:37:52 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // ตั้งค่า URL ของ Google Apps Script Web App ที่นี่
 // ดูวิธี deploy ได้ใน SETUP.md
 // ตัวอย่าง: "https://script.google.com/macros/s/AKfycb.../exec"
-const APPS_SCRIPT_URL = "PASTE_YOUR_APPS_SCRIPT_WEB_APP_URL_HERE";
+const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbxeH3HAdrveKut1YVh50EADSs9f6JH6w7MC-jL0WEcHJSlWFpQAR01izh1GskptjV5zfQ/exec";
